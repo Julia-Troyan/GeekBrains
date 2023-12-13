3a89351f2ad99382303cc146dae1f08f9ef54dfd
Юлия <uliana.troyan@mail.ru>
2023-12-13 21:45:18 +0300

    Код написан

--- Control work/Program.cs
+++ Control work/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        string[] resultArray = GetStringsLengthLessThanOrEqualThree(inputArray);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(string.Join(", ", resultArray));
+    }
+
+    static string[] GetStringsLengthLessThanOrEqualThree(string[] inputArray)
+    {
+        int count = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
+}
